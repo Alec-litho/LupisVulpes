e0769285b191016ea9764b9ec6f9739ec24b70df
Alec-litho <zustzero123@gmail.com>
2024-05-18 00:02:17 +0300

    code logic for slider

---
+++ resources/js/Components/ProjectReview.tsx
@@ -0,0 +1,22 @@
+
+
+
+export default function ProjectReviewComponent() {
+    return (
+        <div className="ProjectReviewComponent">
+                <div className="ProjectReviewComponent__primaryImage">
+                  <img src="./images/projectReviewSlider_images/Home_grown_dogs.png"></img>
+                  <h1 className="ProjectReviewComponent__showName">Home Grown Dogs</h1>
+                </div>
+                <div className="ProjectReviewComponent__secondaryImage">
+                  <img src="./images/projectReviewSlider_images/The_Audience.png"></img>
+                  <h1 className="ProjectReviewComponent__showName">The Audience</h1>
+                </div>
+                <div className="ProjectReviewComponent__tertiaryImage">
+                  <img src="./images/projectReviewSlider_images/Captain_and_his_crew.png"></img>
+                  <h1 className="ProjectReviewComponent__showName">Captain And His Crew</h1>
+                </div>
+                
+        </div>
+    )
+}
