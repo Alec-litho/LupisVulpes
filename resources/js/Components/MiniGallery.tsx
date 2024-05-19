0c298aa05a5faa2f2e14ac0668c9e0821d623d8f
Alec-litho <zustzero123@gmail.com>
2024-05-19 23:17:51 +0300

    make logic for mini gallery

--- resources/js/Components/MiniGallery.tsx
+++ resources/js/Components/MiniGallery.tsx
@@ -1,18 +1,25 @@
+import "../../css/components/MiniGallery.scss"
 
-const testImages = [
-                    "./images/miniGalleryComponent_images/1.png",
-                    "./images/miniGalleryComponent_images/2.png",
-                    "./images/miniGalleryComponent_images/3.png"
-                ]
 
 export default function MiniGalleryComponent() {
+    
+const testImages: [] = [
+    // "./images/miniGalleryComponent_images/1.png",
+    // "./images/miniGalleryComponent_images/2.png",
+    // "./images/miniGalleryComponent_images/3.png"
+]
     return (
         <div className="MiniGalleryComponent">
+            {testImages.length!==0?
             <div className="MiniGalleryComponent__reviewWindow">
-                {testImages.map((imageUrl, indx) => {
-                    return <img src={imageUrl} className={`MiniGalleryComponent__img${indx}`}></img>
-                })}
+{                    testImages.map((imageUrl, indx) => {
+                        return <img src={imageUrl} className={`MiniGalleryComponent__img${indx}`}></img>
+                    })
+                }
             </div>
+            :
+            <div className="MiniGalleryComponent__emptyWindow"></div>
+            }
             <div className="MiniGalleryComponent__menuBar">
                 <div className="menu_header">
                     <h1>Filters</h1>
